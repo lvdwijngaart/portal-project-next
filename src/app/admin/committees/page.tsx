0ffe5c4945eb3178847eef4b538718f62603e328
Luca van der Wijngaart <l.vd.wijngaart@outlook.com>
2025-05-22 13:58:36 +0200

    [Docs] Add docs to the admin pages laying out their functionalities
    All the admin components now have Docs which explain what functionalities the page should include. This will be of great help when stuck or confused in the future

---
+++ src/app/admin/committees/page.tsx
@@ -0,0 +1,30 @@
+
+
+/**
+ * AdminCommitteesPage component
+ * 
+ * Here an admin can manage committees. It should allow the admin to: 
+ * * Committee management:
+ * * * View all committees
+ * * * Add/Edit/Delete committees
+ * * * View committee details
+ * * * Assign/Remove members to/from committees
+ * * Settings: 
+ * * * Manage committee settings (e.g., committee types, categories, fields, etc.)
+ * @todo Devise more detailed requirements for this page.
+ * 
+ * @returns JSX element representing the admin committees page.
+ */
+export default function AdminCommitteesPage() {
+
+
+  return (
+    <div className="admin-page committees-page">
+      <div className="admin-header">
+        {/* Tab component */}
+        <h1>Admin Committees Page</h1> 
+      </div>
+      {/* Add your content here */}
+    </div>
+  );
+}
