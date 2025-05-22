10a7597ac56afe4cbfc73fdc5958c6924cf629db
Luca van der Wijngaart <l.vd.wijngaart@outlook.com>
2025-05-22 16:03:21 +0200

    [Hotfix] Quick fix for build error due to missing types
    Add a Type interface for Team which was first any which caused build errors.

---
+++ src/app/teams/types.ts
@@ -0,0 +1,10 @@
+
+export interface Team {
+  id: number
+  name: string
+  description: string
+  members: Array<{
+    name: string
+  }>
+  // TODO: add the rest of your fields once you know them
+}
