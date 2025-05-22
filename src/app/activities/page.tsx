cca2d4e4b88bb1979a6f87027f0f0504c9e482cc
Luca van der Wijngaart <l.vd.wijngaart@outlook.com>
2025-05-22 14:36:10 +0200

    [Docs] Add doc to Activities page

---
+++ src/app/activities/page.tsx
@@ -0,0 +1,30 @@
+
+
+/**
+ * ActivitiesPage component
+ * 
+ * This page will be used for everything related to activities.
+ * It should allow the user to:
+ * * Activities: 
+ * * * View a list of upcoming activities
+ * * * View details of an (upcoming) activity
+ * * * Sign up for an activity
+ * * * View a list of past activities
+ * * * View details of a past activity (photos)
+ * @todo Devise more detailed requirements for this page.
+ * 
+ * @returns JSX element representing the activities page.
+ */
+export default function ActivitiesPage() {
+
+
+  return (
+    <div className="activities-page">
+      <div className="header">
+        <h1>Activities Page</h1> 
+      </div>
+      <p>This is the activities page.</p>
+      {/* Add your content here */}
+    </div>
+  );
+}
