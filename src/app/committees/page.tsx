6450f9e2bb649eeabdbea504e802e3a67de474ee
Luca van der Wijngaart <l.vd.wijngaart@outlook.com>
2025-05-22 16:01:43 +0200

    [Docs] Add Docs laying out functionalities for activities & committees

---
+++ src/app/committees/page.tsx
@@ -0,0 +1,32 @@
+
+
+/**
+ * CommitteesPage component
+ * 
+ * This page will be used for everything related to committees.
+ * It should allow the user to:
+ * * Committees: 
+ * * * View a list of committees
+ * * * View details and members of a committee
+ * * * View past activities organized by a committee
+ * * Settings: 
+ * * * Manage committee settings
+ * * * Manage committee categories
+ * * * Manage committee types (?)
+ * @todo Devise more detailed requirements for this page.
+ * 
+ * @returns 
+ */
+export default function CommitteesPage() {
+
+
+  return (
+    <div className="committees-page">
+      <div className="header">
+        <h1>Committees Page</h1> 
+      </div>
+      <p>This is the committees page.</p>
+      {/* Add your content here */}
+    </div>
+  );
+}
